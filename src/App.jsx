58c4c563742f614c08af3d666d78063d0bace175
YUVRAJSINGH3112 <yuvrajsingh3112s@gmail.com>
2025-02-01 22:33:39 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ const QuizApp = () => {
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-black p-5">
           {/* Timer */}
           {!quizFinished && (
-            <div className="fixed top-4 right-4 text-black px-4 py-2 rounded bg-red-500 text-white">
+            <div className="fixed top-4 right-4 text-black px-4 py-2 rounded ">
               ⏳ Time Left: {formatTime(timeLeft)}
             </div>
           )}
